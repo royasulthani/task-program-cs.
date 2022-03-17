163586f5742b8c5e6a5e7f2462420114c26dcfce
royasulthani <99806068+royasulthani@users.noreply.github.com>
2022-03-17 01:32:21 -0700

    Add files via upload

---
+++ task group class/task group class/Group.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace task_group_class
+{
+    
+    
+       using System;
+       using System.Collections.Generic;
+       using System.Text;
+
+namespace Group_class
+    {
+        class Group
+        {
+            private string _no;
+            private int _studentlimit;
+
+            public int Studentlimit
+            {
+                get => this._studentlimit;
+                set
+                {
+                    if (value >= 0)
+                    {
+                        this._studentlimit = value;
+                    }
+                }
+            }
+            public string No
+            {
+                get => this._no;
+                set
+                {
+                    if (value.Length > 0 && Num(value))
+                    {
+                        this._no = value;
+                    }
+                }
+            }
+
+            public string no { get; internal set; }
+
+            public bool Num(string str)
+            {
+                if (!string.IsNullOrWhiteSpace(str))
+                {
+                    if (char.IsUpper(str[0]) && char.IsUpper(str[1]))
+                        return true;
+                    if (char.IsDigit(str[2]) && char.IsDigit(str[3]) && char.IsDigit(str[4]))
+                        return true;
+
+
+                }
+                return false;
+            }
+
+
+        }
+    }
+        }
+
+            
+
+    
+       
+
---
+++ task group class/task group class/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using task_group_class.Group_class;
+
+namespace ConsoleApp10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Group group = new Group();
+            group.no = "AP203";
+            Console.WriteLine(group.No);
+        }
+    }
+}
